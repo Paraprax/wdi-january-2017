e018157858d169a3293b569d3a178c1255616eb9
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-02-16 12:22:26 -0500

    Add in-class code for 30-intro-to-ajax

--- 30-intro-to-ajax/ajax.js
+++ 30-intro-to-ajax/ajax.js
@@ -1,3 +1,44 @@
 $(function() {
-
+    $("#load-monsters").on('click', function() {
+		console.log('Before .ajax line');
+		
+		$.ajax({
+			crossDomain: false, // Needed for dataType because monsters-api is not same origin
+			dataType: 'html',
+			url: 'http://monsters-api.herokuapp.com/monsters',
+			//data: { limit: 2 },
+			method: 'GET'
+		}).done(function(data) {
+			$('body').append(data);
+		}).fail(function(jqXHR, textStatus, errorThrown) {
+			console.log('Ajax Request Failed!');
+			console.log(jqXHR);
+		}).always(function() {
+			console.log('Ajax Request Sent!');
+		});
+       
+        console.log('After .ajax line');
+    });
+	
+	$("#add-monster").on('click', function() {
+		$.ajax({
+			//crossDomain: false, // Needed for dataType because monsters-api is not same origin
+			//dataType: 'text',
+			url: 'http://monsters-api.herokuapp.com/monsters',
+			data: { 
+				monster: {
+					name: 'Frankenstein',
+					home: 'Transylvania',
+					creepiness: 15
+				}
+			},
+			method: 'POST'
+		}).done(function() {
+			$('body').append('SUCCESS: Monster created');
+		}).fail(function() {
+			$('body').append('ERROR: Could not create monster');
+		}).always(function() {
+			$('body').append('Add operation complete');
+		});
+	});
 });
