4b2b53f1a0372803fa0814f8eadccfe17af87cca
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-02-17 11:23:04 -0500

    Use respond_to with request.xhr

--- 31-ajax-and-json/robots/app/controllers/robots_controller.rb
+++ 31-ajax-and-json/robots/app/controllers/robots_controller.rb
@@ -9,13 +9,21 @@ class RobotsController < ApplicationController
   def show
     # We don't need to load the robot here, it's done in the load_robot method
     # @robot = Robot.find(params[:id])
-    
-    
-    if request.xhr?
-      render layout: false
+
+    respond_to do |format|
+      format.html do
+        if request.xhr?
+          render layout: false
+        end
+      end
+      
+      format.json do
+        render json: @robot, except: %i(created_at updated_at)
+      end
     end
-    
+
   end
+
 
   private
   # load_robot and robot_params are  in the private section because they aren't
