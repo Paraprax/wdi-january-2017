1f1a949c108cb56be799153547c3ee1b5d6cfbc9
Daniel Moniz <daniel.moniz@gmail.com>
2017-02-22 09:49:34 -0500

    Add work from 32-ajax-and-forms lesson.

--- 32-ajax-and-forms/todos/app/assets/javascripts/greetings.js
+++ 32-ajax-and-forms/todos/app/assets/javascripts/greetings.js
@@ -1,2 +1,33 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(function() {
+
+  $('#new_greeting').submit(function(event) {
+    event.preventDefault();
+    $.ajax({
+      url: $(this).attr('action'),
+      method: $(this).attr('method'),
+      data: $(this).serialize(),
+      dataType: 'json'
+    }).done(function(responseData) {
+      console.log('came back successfully');
+      console.log(responseData);
+
+
+      // var listElement = "<li>" + responseData + "</li>";
+      // var listElement = $('<li>', {
+      //   text: responseData.note + ", created at: " + responseData.created_at,
+      // });
+      // $("#greeting-list").prepend(listElement);
+
+      $("#greeting-list").prepend(responseData);
+
+    }).fail(function() {
+      console.log('failed');
+    }).always(function() {
+      console.log('came back (period)');
+    });
+  });
+
+});
--- 32-ajax-and-forms/todos/app/controllers/greetings_controller.rb
+++ 32-ajax-and-forms/todos/app/controllers/greetings_controller.rb
@@ -7,11 +7,29 @@ class GreetingsController < ApplicationController
 
   def create
     @greeting = Greeting.new(greeting_params)
-    if @greeting.save
-      redirect_to greetings_path
+
+    if request.xhr?
+      @greeting.save
+      # render plain: @greeting.note
+      # render json: @greeting
+      # render json: {'cool': 'awesome', 'greeting': @greeting.note }
+      # render html: "<li>#{@greeting.note}</li>".html_safe
+      # render partial: 'greeting'
+
+      respond_to do |format|
+        format.json { render json: @greeting }
+        format.html { render html: "<li>#{@greeting.note}</li>".html_safe }
+      end
+
     else
-      @greetings = Greeting.all.order(created_at: :desc)
-      render :index
+
+      if @greeting.save
+        redirect_to greetings_path
+      else
+        @greetings = Greeting.all.order(created_at: :desc)
+        render :index
+      end
+
     end
   end
 
